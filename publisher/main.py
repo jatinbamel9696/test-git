017816679446f2178fc884e61478ff42e7dbad0e
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2024-01-12 16:13:51 +0530

    test1

--- publisher/main.py
+++ publisher/main.py
@@ -2,7 +2,7 @@ import requests
 import sys
 
 def test_credentials(login, password):
-    confluence_url = "https://jatin-bamel.atlassian.net/wiki/rest/api/"  # Replace with your Confluence instance URL
+    confluence_url = "https://jatin-bamel.atlassian.net/"  # Replace with your Confluence instance URL
 
     try:
         response = requests.get(confluence_url, auth=(login, password))
