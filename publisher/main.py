dae201af1e1574ed5488a8b80d0646ee67439a67
jatinbamel9696 <45420350+jatinbamel9696@users.noreply.github.com>
2024-01-12 16:56:58 +0530

    test

--- publisher/main.py
+++ publisher/main.py
@@ -1,27 +1,34 @@
-import requests
-import sys
+import argparse
+import logging
+from config.getconfig import getConfig
+from pagesController import searchPages, deletePages, createPage
 
-def test_credentials(login, password):
-    confluence_url = "https://jatin-bamel.atlassian.net/"  # Replace with your Confluence instance URL
+logging.basicConfig(level=logging.INFO)
 
-    try:
-        response = requests.get(confluence_url, auth=(login, password))
-        response.raise_for_status()
+# Parse arguments with LOGIN and PASSWORD for Confluence
+parser = argparse.ArgumentParser()
+parser.add_argument('--login', help='Login with "" is mandatory', required=True)
+parser.add_argument('--password', help='Password with "" is mandatory', required=True)
+parser.add_argument('--release-notes', help='Path to release notes file', required=True)
+args = parser.parse_args()
+inputArguments = vars(args)
 
-        # Check if the response contains expected data indicating successful authentication
-        if "some_expected_data" in response.text:
-            print("Credentials are valid.")
-        else:
-            print("Error: Unexpected response content.")
-            sys.exit(1)
+CONFIG = getConfig()
 
-    except requests.exceptions.RequestException as err:
-        print(f"Error: {err}")
-        sys.exit(1)
+logging.debug(CONFIG)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python test_credentials.py <login> <password>")
-        sys.exit(1)
+# Search for the Confluence page named "jatin-test"
+search_result = searchPages(login=inputArguments['login'], password=inputArguments['password'], title="jatin-test")
 
-    test_credentials(login=sys.argv[1], password=sys.argv[2])
+# If the page exists, delete it
+if search_result:
+    deletePages(pagesIDList=search_result, login=inputArguments['login'], password=inputArguments['password'])
+
+# Create a new Confluence page with release notes content
+release_notes_content = ""
+with open(inputArguments['release_notes'], 'r', encoding="utf-8") as release_notes_file:
+    release_notes_content = release_notes_file.read()
+
+# Create a new Confluence page with the release notes content
+createPage(title="jatin-test", content=release_notes_content, parentPageID=None,
+           login=inputArguments['login'], password=inputArguments['password'])
